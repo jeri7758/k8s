e6553f8dbea0d63bc8c995ddf3b36960a1e8ca52
jeri <jareer@jeri>
2022-06-20 01:47:16 +0530

    kub_deploy.sh addeddasdsdsdasadsad

--- kub_deploy.sh
+++ kub_deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 ssh -o StrictHostKeychecking=no jeri@34.93.4.5 \
-"sed -i 's,tagname,44,g' deploy.yaml && \
+"sed -i\ 's\tagname\44\g' deploy.yaml && \
 sudo cp -av deploy.yaml /var/lib/jenkins/workspace/kub_test/deployment.yaml"
