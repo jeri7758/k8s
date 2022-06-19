e303b6c45c919073fda1d74889942d39e1ead1ba
jeri <jeri@jeri>
2022-06-19 10:30:03 +0530

    Jenkinsfile modified again5

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-ssh -o StrictHostKeychecking=no jeri@${SERVER_IP} && \
-COMMIT=$(git rev-pasrse --verify HEAD) && \
+ssh -o StrictHostKeychecking=no jeri@34.93.4.5 \
+'sudo cp -av /var/lib/jenkins/workspace/kub_test . &&
+COMMIT=$(git rev-parse --verify HEAD) && \
 sudo docker image build -t jerijs/kub_project . \
---build-arg "app_name=kub_project"
--t "jerijs/kub_project:latest"
--t "jerijs/kub_project:${commit}"
+--build-arg "app_name=kub_project" \
+-t "jerijs/kub_project:latest" \
+-t "jerijs/kub_project:${commit}"'
