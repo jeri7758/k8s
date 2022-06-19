88de1c26f4010a171f3bf87a18610104c0c13897
jeri <jareer@jeri>
2022-06-19 16:21:36 +0530

    docker_last1

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -2,8 +2,7 @@
 ssh -o StrictHostKeychecking=no jeri@34.93.4.5 \
 'sudo cp -av /var/lib/jenkins/workspace/kub_test . &&
 sudo chown -R jeri. kub_test && cd /home/jeri/kub_test && \
-COMMIT=$(git rev-parse --verify HEAD) && \
+#COMMIT=$(git rev-parse --verify HEAD) && \
 sudo docker image build -t jerijs/kub_project . \
 --build-arg "app_name=kub_project" \
--t "jerijs/kub_project:latest" \
--t "jerijs/kub_project:$COMMIT"'
+-t "jerijs/kub_project:latest"'
