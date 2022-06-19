f27621bbb618fe897d56d229a4f5c48f13b03ce7
jeri <jeri@jeri>
2022-06-19 10:42:29 +0530

    Jenkinsfile modified again7

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ssh -o StrictHostKeychecking=no jeri@34.93.4.5 \
 'sudo cp -av /var/lib/jenkins/workspace/kub_test . &&
+chown -R jeri. kub_test
 COMMIT=$(git rev-parse --verify HEAD) && \
 sudo docker image build -t jerijs/kub_project . \
 --build-arg "app_name=kub_project" \
